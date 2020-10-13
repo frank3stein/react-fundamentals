eb1b29c6891f1b1d0b3a2a3021da67dabb06e9f7
Emil Martinov <martinov.emil@gmail.com>
2020-10-13 13:02:21 +0200

    Workshop finished

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,18 +12,112 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+
+// 1. Solution inline coding, no custom component
+// const smallBox = <div className="box--small" style={{
+//   backgroundColor: 'lightblue',
+//   fontStyle: 'italic'
+// }}> small lightblue box</div >
+// const mediumBox = <div className="box--medium box" style={{
+//   backgroundColor: 'pink',
+//   fontStyle: 'italic'
+// }}>medium pink box</div>
+// const largeBox = <div className="box--large box" style={{
+//   backgroundColor: 'orange',
+//   fontStyle: 'italic'
+// }}>large orange box</div>
+
+// function App() {
+//   return (
+//     <div>
+//       {smallBox}
+//       {mediumBox}
+//       {largeBox}
+//     </div>
+//   )
+// }
+
+// 2. Create Custom Box Component
+
+// const Box = ({ className, style, children }) => (
+//   <div className={className} style={style}>{children}</div>
+// )
+
+
+// function App() {
+//   return (
+//     <div>
+//       <Box className='box--small' style={{
+//         backgroundColor: 'lightblue',
+//         fontStyle: 'italic'
+//       }}> small lightblue box</Box>
+//       {mediumBox}
+//       {largeBox}
+//     </div>
+//   )
+// }
+
+// 3.Create an API for box size
+
+// const Box = ({ className, style, children }) => {
+//   switch (className) {
+//     case 'small':
+//       className = 'box--small box'
+//       break;
+//     case 'medium':
+//       className = 'box--medium box'
+//       break;
+//     case 'large':
+//       className = 'box--large box'
+//       break;
+//     default:
+//       className = '';
+//   }
+//   return <div className={className} style={style}>{children}</div>
+// }
+
+
+// function App() {
+//   return (
+//     <div>
+//       <Box className='large' style={{
+//         backgroundColor: 'lightblue',
+//         fontStyle: 'italic'
+//       }}> small lightblue box</Box>
+//       {mediumBox}
+//       {largeBox}
+//     </div>
+//   )
+// }
+
+
+// Final Solution
+
+const Box = ({ className, style, size, ...restProps }) => {
+  const sizeClassName = size ? `box--${size}` : ''
+  return <div
+    className={`box ${sizeClassName}`}
+    style={{
+      fontSize: 'italic',
+      ...style
+    }}
+    {...restProps}
+  ></div >
+}
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box size='small' style={{
+        backgroundColor: 'lightblue',
+      }}> small lightblue box</Box>
+      <Box size="medium" style={{
+        backgroundColor: 'pink'
+      }}> medium pink box</Box>
+      <Box size="large" style={{
+        backgroundColor: 'orange'
+      }}> large orange box</Box>
     </div>
   )
 }
-
 export default App
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,9 +1,9 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
-function UsernameForm({onSubmitUsername}) {
+function UsernameForm({ onSubmitUsername }) {
   // 🐨 add a submit event handler here (`handleSubmit`).
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
@@ -17,17 +17,56 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 add the onSubmit handler to the <form> below
 
   // 🐨 make sure to associate the label to the input by specifying an `id` on
-  // the input and a matching value as an `htmlFor` prop on the label.
+  // the input  and a matching value as an `htmlFor` prop on the label.
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // console.log("input ref ", inputRef.current.value)
+    // onSubmitUsername(e.target.elements.username.value)
+    // onSubmitUsername(inputRef.current.value)
+    onSubmitUsername(username)
+
+  }
+
+
+
+  const inputRef = useRef();
+
+  // 2
+
+  // const [username, setUsername] = useState('')
+  // const [error, setError] = useState(null)
+
+  // const handleInput = e => {
+  //   console.log(e.target.value)
+  //   const inputValue = e.target.value
+  //   const isLowerCase = inputValue === inputValue.toLowerCase();
+  //   setUsername(inputValue)
+  //   setError(isLowerCase ? null : 'Username must be lowercase')
+  // }
+
+  // 3 
+  const [username, setUsername] = useState('')
+  const error = null // to shut the errors
+  const handleInput = e => {
+    console.log(e.target.value)
+    setUsername(e.target.value.toLowerCase())
+  }
+
+
+  // htmlFor is needed for screenreaders, and also when clicked on the label, it has to know which field it should focus on. If has to be htmlFor since for is reserved in javascript. 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <label htmlFor="username">Username:</label>
+        <input type="text" id='username' ref={inputRef} onChange={handleInput} value={username} />
       </div>
-      <button type="submit">Submit</button>
+      <button disabled={Boolean(error)} type="submit">Submit</button>
     </form>
   )
 }
+
+// We use value to update the sate onto the component, so even if it is caps on, it will always write lowercase
 
 function App() {
   const onSubmitUsername = username => alert(`You entered: ${username}`)
